002c1ebe3370687e6bf14f6ed01796500173fe8e
ZoRoEmiya <davidlevi14404@gmail.com>
2026-04-18 20:37:14 +0300

    feat: add initial battle engine files and type chart for game mechanics

---
+++ server/battle/effects.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "Focus Boost": { type: "buff", stat: "atk", amount: 1 },
+  "Guard Up": { type: "buff", stat: "def", amount: 1 },
+  "Speed Up": { type: "buff", stat: "spd", amount: 1 },
+  "Recover": { type: "heal", amount: 0.3 }
+};
---
+++ server/battle/typeChart.js
@@ -0,0 +1,27 @@
+module.exports = {
+  Fire: {
+    Grass: 2,
+    Water: 0.5,
+    Fire: 0.5
+  },
+  Water: {
+    Fire: 2,
+    Electric: 0.5,
+    Water: 0.5
+  },
+  Grass: {
+    Water: 2,
+    Fire: 0.5,
+    Grass: 0.5
+  },
+  Electric: {
+    Water: 2,
+    Earth: 0,
+    Electric: 0.5
+  },
+  Earth: {
+    Electric: 2,
+    Grass: 0.5
+  },
+  Normal: {}
+};
