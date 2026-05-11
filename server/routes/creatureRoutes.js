59cf5e2b55e3192229f80fc94b7e915ee3da76a3
ZoRoEmiya <davidlevi14404@gmail.com>
2026-05-11 08:23:52 +0300

    feat: update @prisma/adapter-better-sqlite3 to version 7.8.0 and configure PrismaClient with adapter

--- server/routes/creatureRoutes.js
+++ server/routes/creatureRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
 
 const router = express.Router();
-const prisma = new PrismaClient();
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./dev.db"
+});
+
+const prisma = new PrismaClient({
+  adapter
+});
 
 router.get("/", async (req, res) => {
   try {
